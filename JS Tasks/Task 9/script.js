36961081bd4650664d2f5e43a055d41671e10102
Mindaugas <109964675+Kameleonas@users.noreply.github.com>
2022-11-15 22:58:58 +0200

    Task 9 completed

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -9,3 +9,18 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+// constructor function for Movies ES6
+class Movie {
+  constructor(title, director, budget) {
+    (this.title = title), (this.director = director), (this.budget = budget);
+  }
+  get cost() {
+    return this.wasExpensive();
+  }
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+const newMovie = new Movie("titanikas", "anonymous", 500000000);
+console.log(newMovie);
