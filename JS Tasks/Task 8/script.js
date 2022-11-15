8ffc2bcb6d01de3aa7d89478260133968ab15da1
Mindaugas <109964675+Kameleonas@users.noreply.github.com>
2022-11-15 22:39:58 +0200

    Task 8 completed

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,18 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+// Constructor Calculator function
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+  this.newResultsObject = {
+    sum: this.a + this.b,
+    subtraction: this.a - this.b,
+    multiplication: this.a * this.b,
+    division: this.a / this.b,
+  };
+}
+
+const resultone = new Calculator(50, 5);
+console.log(resultone.newResultsObject);
